1ee802c65ff2f14b7b8a912fd9302d38294404dc
Honza Vanek <vanek.honza@gmail.com>
2018-02-11 20:48:21 +0100

    Initial commit

---
+++ src/First.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.Math;
+import static java.lang.Math.*;
+
+class First {
+   private static Random random = new Random();
+   private static Scanner input = new Scanner(System.in);
+    
+   public static void main(String[] args) {
+      
+   }
+}

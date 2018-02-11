08a47d15a9cd633fad2fb37abfdcaf9a9d92338a
Honza Vanek <vanek.honza@gmail.com>
2018-02-11 20:53:04 +0100

    Fix indentation

--- src/First.java
+++ src/First.java
@@ -3,10 +3,10 @@ import java.lang.Math;
 import static java.lang.Math.*;
 
 class First {
-   private static Random random = new Random();
-   private static Scanner input = new Scanner(System.in);
-    
-   public static void main(String[] args) {
-      
-   }
+	private static Random random = new Random();
+	private static Scanner input = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		
+	}
 }
